b7a9d2306ece24d6b22005061833b936ed467e5f
NithinTraining <nithin.pj@spericorn.com>
2021-03-12 09:51:26 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const productRoutes = require("./route/products");
+
+
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/image",
+  {
+    useNewUrlParser:true,
+    useFindAndModify:false,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+  }
+  
+).then(console.log("DB connected"))
+
+
+
+
+app.use(morgan("dev"));
+app.use('/uploads', express.static('uploads'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+// Routes which should handle requests
+app.use("/products", productRoutes);
+
+
+
+const port = process.env.PORT || 4000;
+app.listen(port,()=>console.log("server is running"))
---
+++ route/products.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+      cb(null, new Date().toISOString() + file.originalname);
+    }
+  });
+
+  const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    
+    }
+  };
+
+  const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+  });
+  
+  const Product = require("../models/product");
+  
+  router.get("/", (req, res, next) => {
+    Product.find()
+      .select("name price _id productImage")
+      .exec()
+      .then(docs => {
+        const response = {
+          count: docs.length,
+          products: docs.map(doc => {
+            return {
+              name: doc.name,
+              price: doc.price,
+              productImage: doc.productImage,
+              _id: doc._id,
+              request: {
+                type: "GET",
+                url: "http://localhost:3000/products/" + doc._id
+              }
+            };
+          })
+        };
+        //   if (docs.length >= 0) {
+        res.status(200).json(response);
+        //   } else {
+        //       res.status(404).json({
+        //           message: 'No entries found'
+        //       });
+        //   }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
+
+  router.post("/", upload.single('productImage'), (req, res, next) => {
+    
+
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price,
+        productImage: req.file.path 
+      });
+      product
+        .save()
+        .then(result => {
+         
+         res.send(result)
+            
+          })
+        
+        
+        //res.send(req.files)
+    });
+
+    router.post("/img", upload.array('productImage',10), (req, res, next) => {
+console.log(req.files)
+res.send("multiple file uploaded sucessfully")
+    })
+    
+    module.exports = router;
